bfb39f7e16fbbbfd7e9bf89caf057efa3699ae68
nddat1811 <nddat1811@gmail.com>
2022-07-02 14:52:11 +0700

    service

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/gin-gonic/gin v1.8.1
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/joho/godotenv v1.4.0
 	gorm.io/driver/mysql v1.3.4
 	gorm.io/gorm v1.23.7
@@ -20,12 +21,13 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mashingan/smapping v0.1.14
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
 	golang.org/x/text v0.3.6 // indirect
---
+++ service/auth-service.go
@@ -0,0 +1,72 @@
+package service
+
+import (
+	"log"
+	"github.com/mashingan/smapping"
+	"github.com/nddat1811/simple_project_golang/dto"
+	"github.com/nddat1811/simple_project_golang/entity"
+	"github.com/nddat1811/simple_project_golang/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
+//AuthService is a contract about sth that this service can do
+type AuthService interface {
+	VerifyCredential(email string, password string) interface {}
+	CreateUser(user dto.RegisterDTO) entity.User
+	FindByEmail(email string) entity.User
+	IsDuplicateEmail(email string) bool
+}
+
+type authService struct {
+	userReposity repository.UserRepository
+}
+
+//NewAuthService creates a new instance of AuthService
+func NewAuthService(userRep repository.UserRepository) AuthService {
+	return &authService{
+		userReposity: userRep,
+	}
+}
+
+func (service *authService) VerifyCredential(email string, password string) interface{} {
+	res:= service.userReposity.VerifyCredential(email, password)
+	if v, ok := res.(entity.User); ok {
+		comparedPassword := comparedPassword(v.Password, []byte(password))
+		if v.Email == email && comparedPassword {
+			return res
+		}
+		return false
+	}
+	return false
+}
+
+func (service *authService) CreateUser(user dto.RegisterDTO) entity.User {
+	userToCreate := entity.User{}
+
+	err := smapping.FillStruct(&userToCreate, smapping.MapFields(&user))
+	if err != nil {
+		log.Fatalf("Failed map %v", err)
+	}
+	res := service.userReposity.InsertUser(userToCreate)
+	return res
+}
+
+func (service *authService) FindByEmail(email string) entity.User {
+	return service.userReposity.FindByEmail(email)
+}
+
+func (service *authService) IsDuplicateEmail(email string) bool {
+	res:= service.userReposity.IsDuplicateEmail(email) 
+	return !(res.Error == nil)
+}
+
+func comparedPassword(hashedPwd string, plainPassword []byte) bool {
+	byteHash := []byte(hashedPwd)
+	err := bcrypt.CompareHashAndPassword(byteHash, plainPassword)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
+}
+
